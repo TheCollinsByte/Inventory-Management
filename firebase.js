2e79de0b0581b7eab0f880c8ec8774f6b3121132
colspan <collin@collindigm.com>
2024-08-04 12:55:58 +0300

    Firebase config file clean-up

--- firebase.js
+++ firebase.js
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: process.env.API_KEY_,
   authDomain: process.env.AUTH_DOMAIN_,
@@ -17,7 +12,6 @@ const firebaseConfig = {
   measurementId: process.env.MEASUREMENT_ID_,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
 
